303e5dc63fe9e807246db6fd6b87c449ef791f5a
Paulo <161053846+IIIasterIII@users.noreply.github.com>
2025-12-18 21:50:55 +0100

    feat: backend init 2

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
 async def home():
     return {"Data": "Hello World!"}
+
+if __name__ == "__main__":
+    uvicorn.run(host="localhost", port=8000, reload=True)
