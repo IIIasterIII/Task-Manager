c72eb244c8df8b4f06fc51f1b00f6ca167fc2d98
Paulo <161053846+IIIasterIII@users.noreply.github.com>
2025-12-19 19:53:31 +0100

    feat: Redis + JWT + (not finished auth) + bugs with cookie on frontend + Refresh Token on Redis

---
+++ nextjs-frontend/app/auth/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import AuthForm from "../components/ui/authForm"
+
+const Page = () => {
+  return (
+    <div>
+        <AuthForm/>
+    </div>
+  )
+}
+
+export default Page
--- nextjs-frontend/app/page.tsx
+++ nextjs-frontend/app/page.tsx
@@ -1,6 +1,43 @@
-import Image from "next/image";
+"use client"
 
+import Image from "next/image";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const cookies = document.cookie; // теперь доступна access_token
+        const accessToken = cookies
+          .split("; ")
+          .find(row => row.startsWith("access_token="))
+          ?.split("=")[1];
+        console.log(accessToken)
+  
+        const res = await fetch("http://localhost:8000/me", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (res.status == 401) {
+
+          const res = await fetch("http://localhost:8000/refresh", {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+
+          console.log(res)
+
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    };
+  
+    checkAuth();
+  }, []);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
